57e741f52ade89621d6eb3b279aa7f0dd86ecb80
vijaysainunovus <188815020+vijaysainunovus@users.noreply.github.com>
2026-01-11 14:50:16 -0500

    chore: upgrade project to UE 5.7 (BuildSettings V6)

--- Source/ScarletSandsEditor.Target.cs
+++ Source/ScarletSandsEditor.Target.cs
@@ -8,7 +8,7 @@ public class ScarletSandsEditorTarget : TargetRules
 	public ScarletSandsEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 		ExtraModuleNames.Add("ScarletSands");
 	}
